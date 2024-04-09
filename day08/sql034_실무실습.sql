5f3d39ebd3f140c41024c2e9f7ccda51f23263ba
leekminxx <158007500+leekminxx@users.noreply.github.com>
2024-04-09 14:36:34 +0900

    마무리

--- day08/sql034_실무실습.sql
+++ day08/sql034_실무실습.sql
@@ -43,20 +43,5 @@ SELECT LAST_NAME AS 'e and o Name'
   FROM employees
  WHERE LAST_NAME LIKE '%e%' OR LAST_NAME LIKE  'o%'
 
-/* 5. 현재의 날짜 타입을 날짜 함수를 통해서 확인
-    2006년 5월 20일 부터 2007년 5월20일 사이에 고용된 사원의 이름(First + Last) , 사원번호 , 고용일자
-    단 , 입사일 빠른 순으로 정렬하시오(18개행) */
-SELECT GETDATE();
-
-
-
-
-
-
-
-
-
-
-
 
 
